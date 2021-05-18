4208f9e1dfdbd464056365792d45637395989b54
Angel <angel.1999barsa@gmail.com>
2021-05-18 14:28:25 -0500

    '18'

---
+++ bdcitas.sql
@@ -0,0 +1,273 @@
+/*
+SQLyog Ultimate v11.11 (64 bit)
+MySQL - 5.5.5-10.4.18-MariaDB : Database - bdcitas
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`bdcitas` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+
+USE `bdcitas`;
+
+/*Table structure for table `chat_messages` */
+
+DROP TABLE IF EXISTS `chat_messages`;
+
+CREATE TABLE `chat_messages` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `chat_room_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `message` mediumtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `titulo` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=55 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+/*Data for the table `chat_messages` */
+
+LOCK TABLES `chat_messages` WRITE;
+
+insert  into `chat_messages`(`id`,`chat_room_id`,`user_id`,`message`,`created_at`,`updated_at`,`titulo`) values (1,1,2,'Buen dia!! Cobran envio?','2021-02-28 05:41:16','2021-02-28 05:41:16','Nuevo'),(8,1,2,'El envio es gratis en compras mayores a $500. Solo en mexico.\r\n','2021-02-28 09:25:19','2021-02-28 09:25:19','New'),(9,1,2,'probando','2021-02-28 09:25:28','2021-02-28 09:25:28','Titulo'),(10,1,1,'Respuesta\r\n','2021-02-28 10:10:27','2021-02-28 10:10:27','Tittle'),(11,1,1,'Tengo una Duda!','2021-02-28 11:16:31','2021-02-28 11:16:31','NewTittle'),(13,1,59,'Buenos dias','2021-02-28 13:21:27','2021-02-28 13:21:27',NULL),(14,1,59,'Todavia tienen blusas para niña?','2021-02-28 13:23:31','2021-02-28 13:23:31',NULL),(52,1,1,'tcontenido','2021-04-16 21:13:02','2021-04-16 21:13:02','Nose cual es'),(53,1,1,'perrona!!!','2021-04-16 21:17:36','2021-04-16 21:17:36','Otra historia');
+
+UNLOCK TABLES;
+
+/*Table structure for table `chat_rooms` */
+
+DROP TABLE IF EXISTS `chat_rooms`;
+
+CREATE TABLE `chat_rooms` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+/*Data for the table `chat_rooms` */
+
+LOCK TABLES `chat_rooms` WRITE;
+
+insert  into `chat_rooms`(`id`,`name`,`created_at`,`updated_at`) values (1,'General',NULL,NULL),(2,'Tech Talk',NULL,NULL);
+
+UNLOCK TABLES;
+
+/*Table structure for table `failed_jobs` */
+
+DROP TABLE IF EXISTS `failed_jobs`;
+
+CREATE TABLE `failed_jobs` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `uuid` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `connection` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `queue` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `exception` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `failed_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `failed_jobs_uuid_unique` (`uuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+/*Data for the table `failed_jobs` */
+
+LOCK TABLES `failed_jobs` WRITE;
+
+UNLOCK TABLES;
+
+/*Table structure for table `migrations` */
+
+DROP TABLE IF EXISTS `migrations`;
+
+CREATE TABLE `migrations` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+/*Data for the table `migrations` */
+
+LOCK TABLES `migrations` WRITE;
+
+insert  into `migrations`(`id`,`migration`,`batch`) values (1,'2014_10_12_000000_create_users_table',1),(2,'2014_10_12_100000_create_password_resets_table',1),(3,'2014_10_12_200000_add_two_factor_columns_to_users_table',1),(4,'2019_08_19_000000_create_failed_jobs_table',1),(5,'2019_12_14_000001_create_personal_access_tokens_table',1),(6,'2020_05_21_100000_create_teams_table',1),(7,'2020_05_21_200000_create_team_user_table',1),(8,'2020_05_21_300000_create_team_invitations_table',1),(9,'2021_02_15_174857_create_sessions_table',1);
+
+UNLOCK TABLES;
+
+/*Table structure for table `password_resets` */
+
+DROP TABLE IF EXISTS `password_resets`;
+
+CREATE TABLE `password_resets` (
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  KEY `password_resets_email_index` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+/*Data for the table `password_resets` */
+
+LOCK TABLES `password_resets` WRITE;
+
+UNLOCK TABLES;
+
+/*Table structure for table `personal_access_tokens` */
+
+DROP TABLE IF EXISTS `personal_access_tokens`;
+
+CREATE TABLE `personal_access_tokens` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `tokenable_type` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tokenable_id` bigint(20) unsigned NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `abilities` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `last_used_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `personal_access_tokens_token_unique` (`token`),
+  KEY `personal_access_tokens_tokenable_type_tokenable_id_index` (`tokenable_type`,`tokenable_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+/*Data for the table `personal_access_tokens` */
+
+LOCK TABLES `personal_access_tokens` WRITE;
+
+UNLOCK TABLES;
+
+/*Table structure for table `sessions` */
+
+DROP TABLE IF EXISTS `sessions`;
+
+CREATE TABLE `sessions` (
+  `id` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `user_id` bigint(20) unsigned DEFAULT NULL,
+  `ip_address` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `user_agent` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `payload` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `last_activity` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `sessions_user_id_index` (`user_id`),
+  KEY `sessions_last_activity_index` (`last_activity`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+/*Data for the table `sessions` */
+
+LOCK TABLES `sessions` WRITE;
+
+insert  into `sessions`(`id`,`user_id`,`ip_address`,`user_agent`,`payload`,`last_activity`) values ('wAOkwi2ryq3oNyAlvGw3XgtN4cForT9w7ZN5dqH8',NULL,'127.0.0.1','Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36','YTozOntzOjY6Il90b2tlbiI7czo0MDoiRzh2enRkalI0MWpleWFYZGVEQ2FGTmwwaVFTRWdnMFZlemZnSmdzZSI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6Mjk6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMC9Db250YWN0Ijt9czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319fQ==',1620438514),('ysI6UKz5QxdhRD6xkKd2uLgoVW1g4A3ZYgcEXt5d',NULL,'127.0.0.1','Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36','YTozOntzOjY6Il90b2tlbiI7czo0MDoiRERxaGRUY1BBNDRrcHE2RWJJb3hIUXVhcmZmVEZyNnJPQXFrZGpqUyI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6Mjc6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMC9sb2dpbiI7fXM6NjoiX2ZsYXNoIjthOjI6e3M6Mzoib2xkIjthOjA6e31zOjM6Im5ldyI7YTowOnt9fX0=',1620439731);
+
+UNLOCK TABLES;
+
+/*Table structure for table `team_invitations` */
+
+DROP TABLE IF EXISTS `team_invitations`;
+
+CREATE TABLE `team_invitations` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `team_id` bigint(20) unsigned NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `role` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `team_invitations_team_id_email_unique` (`team_id`,`email`),
+  CONSTRAINT `team_invitations_team_id_foreign` FOREIGN KEY (`team_id`) REFERENCES `teams` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+/*Data for the table `team_invitations` */
+
+LOCK TABLES `team_invitations` WRITE;
+
+UNLOCK TABLES;
+
+/*Table structure for table `team_users` */
+
+DROP TABLE IF EXISTS `team_users`;
+
+CREATE TABLE `team_users` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `team_id` bigint(20) unsigned NOT NULL,
+  `user_id` bigint(20) unsigned NOT NULL,
+  `role` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `team_user_team_id_user_id_unique` (`team_id`,`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+/*Data for the table `team_users` */
+
+LOCK TABLES `team_users` WRITE;
+
+insert  into `team_users`(`id`,`team_id`,`user_id`,`role`,`created_at`,`updated_at`) values (1,1,1,'admin',NULL,NULL);
+
+UNLOCK TABLES;
+
+/*Table structure for table `teams` */
+
+DROP TABLE IF EXISTS `teams`;
+
+CREATE TABLE `teams` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` bigint(20) unsigned NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `personal_team` tinyint(1) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `teams_user_id_index` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+/*Data for the table `teams` */
+
+LOCK TABLES `teams` WRITE;
+
+insert  into `teams`(`id`,`user_id`,`name`,`personal_team`,`created_at`,`updated_at`) values (1,1,'Adriana\'s Team',1,'2021-04-16 18:42:23','2021-04-16 18:42:23'),(2,5,'Angel\'s Team',1,'2021-04-16 23:30:13','2021-04-16 23:30:13'),(3,6,'Angel\'s Team',1,'2021-04-18 15:05:08','2021-04-18 15:05:08'),(4,8,'Angel\'s Team',1,'2021-04-21 14:39:30','2021-04-21 14:39:30'),(5,9,'Angel\'s Team',1,'2021-04-21 21:25:07','2021-04-21 21:25:07'),(6,10,'Angel\'s Team',1,'2021-04-21 21:32:11','2021-04-21 21:32:11'),(7,11,'Angel\'s Team',1,'2021-04-21 21:36:40','2021-04-21 21:36:40'),(8,12,'Angel\'s Team',1,'2021-04-21 21:38:36','2021-04-21 21:38:36'),(9,13,'Angel\'s Team',1,'2021-04-22 17:03:12','2021-04-22 17:03:12'),(10,14,'Angel\'s Team',1,'2021-04-22 17:12:55','2021-04-22 17:12:55'),(11,15,'Angel\'s Team',1,'2021-04-22 17:17:35','2021-04-22 17:17:35'),(12,16,'Angel\'s Team',1,'2021-04-22 17:20:43','2021-04-22 17:20:43'),(13,17,'Angel\'s Team',1,'2021-04-22 17:21:54','2021-04-22 17:21:54');
+
+UNLOCK TABLES;
+
+/*Table structure for table `users` */
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email_verified_at` timestamp NULL DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `two_factor_secret` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `two_factor_recovery_codes` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `current_team_id` bigint(20) unsigned DEFAULT NULL,
+  `profile_photo_path` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `desc` varchar(250) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `edad` int(2) DEFAULT NULL,
+  `sexo` char(1) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `estado` varchar(30) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `users_email_unique` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+/*Data for the table `users` */
+
+LOCK TABLES `users` WRITE;
+
+insert  into `users`(`id`,`name`,`email`,`email_verified_at`,`password`,`two_factor_secret`,`two_factor_recovery_codes`,`remember_token`,`current_team_id`,`profile_photo_path`,`created_at`,`updated_at`,`desc`,`edad`,`sexo`,`estado`) values (1,'Adriana Cruz','Adriana@gmail.com',NULL,'$2y$10$hSL2ozmRYN.hpwahTgSGVulrl6xcfUBZtCxWEJ5Sm7IOSZR2HODme',NULL,NULL,NULL,1,'/storage/img/ZiL0WVqXg0Y9d95u5uyP84ZJybBfkQUBu9H35zGy.png','2021-04-16 18:42:23','2021-04-16 18:44:04',NULL,20,'F','Hidalgo'),(2,'Ricardo Perez','Rica@gmail.com',NULL,'',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,25,'M','Yucatan');
+
+UNLOCK TABLES;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
